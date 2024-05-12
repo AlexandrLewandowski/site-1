b3cfb56c7c9c772d0aa1fa24ae56f4946332b270
Alexandr Lewandowski <158829441+AlexandrLewandowski@users.noreply.github.com>
2024-05-12 22:20:32 +0300

    12.05.2024

---
+++ .history/js/main_20240512211752.js
@@ -0,0 +1,16 @@
+$(function () {
+
+    $('.carousel__inner').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 3,
+        responsive: [{
+            breakpoint: 841,
+            settings: {
+                slidesToShow: 2,
+            }
+        }]
+    });
+
+
+});
